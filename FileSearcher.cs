1726d2c589479527cd2ab7e13175ea25d2da7150
Yurii Ivankiv <yutaivankiv102@gmail.com>
2023-07-13 20:55:44 +0300

    FileSearcher

---
+++ FileSearcher.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class FileSearcher
+    {
+        private string folderPath;
+        private string searchPattern;
+
+        public FileSearcher(string _folderPath, string _searchPattern)
+        {
+            this.folderPath = _folderPath;
+            this.searchPattern = _searchPattern;
+        }
+
+        public string[] SearchFiles()//Метод для завдання 1 
+        {
+            return Directory.GetFiles(folderPath, searchPattern);
+        }
+        public string[] SearchFilesMax()//Метод для завдання 2
+        {
+            return Directory.GetFiles(folderPath, searchPattern, SearchOption.AllDirectories);
+        }
+        public void DeleteFiles()//Метод для завдання 3 
+        {
+            string[] files = SearchFiles();
+
+            if (files.Length > 0)
+            {
+                Console.WriteLine("Видаленi файли:");
+
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                    Console.WriteLine(file);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файли з такою маскою не знайдено.");
+            }
+        }
+        public void DeleteFilesInFolder(string folder)// Для завдання 4
+        {
+            string[] files = Directory.GetFiles(folder, searchPattern);
+            string[] subdirectories = Directory.GetDirectories(folder);
+
+            if (files.Length > 0)
+            {
+                Console.WriteLine("Видаленi файли з папки {0}:", folder);
+
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                    Console.WriteLine(file);
+                }
+            }
+
+            foreach (string subdirectory in subdirectories)
+            {
+                DeleteFilesInFolder(subdirectory);
+            }
+            DeleteEmptySubfolders(folder);
+        }
+        private void DeleteEmptySubfolders(string folder)//Метод для завдання 5 
+        {
+            string[] subdirectories = Directory.GetDirectories(folder);
+
+            foreach (string subdirectory in subdirectories)
+            {
+                DeleteEmptySubfolders(subdirectory);
+
+                if (Directory.GetFiles(subdirectory).Length == 0 && Directory.GetDirectories(subdirectory).Length == 0)
+                {
+                    Directory.Delete(subdirectory);
+                    Console.WriteLine("Видалена пiдпапка: {0}", subdirectory);
+                }
+            }
+        }
+        public void DisplayFolderStructure()//Для завдання 5
+        {
+            DisplayStructure(folderPath, "");
+        }
+        private void DisplayStructure(string folder, string indent)//Для завдання 5
+        {
+            Console.WriteLine(indent + folder);
+
+            string[] files = Directory.GetFiles(folder, searchPattern);
+            foreach (string file in files)
+            {
+                Console.WriteLine(indent + "├─" + Path.GetFileName(file));
+            }
+
+            string[] subdirectories = Directory.GetDirectories(folder);
+            foreach (string subdirectory in subdirectories)
+            {
+                Console.WriteLine(indent + "├─" + Path.GetFileName(subdirectory) + " [папка]");
+                DisplayStructure(subdirectory, indent + "│  ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp12;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть шлях до папки:"); //C:\Users\Admin\Desktop\Test
+        string folderPath = Console.ReadLine();
+        //string folderPath = (@"C:\Users\Admin\Desktop\Test"); 
+        Console.WriteLine("Введiть маску для пошуку:");
+        string searchPattern = Console.ReadLine(); //*.txt
+        //string searchPattern = "*.txt";
+        FileSearcher fileSearcher = new FileSearcher(folderPath, searchPattern);
+        string[] files = fileSearcher.SearchFilesMax();
+        if (files.Length > 0)
+        {
+            Console.WriteLine("Знайденi файли:");
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файли з такою маскою не знайдено.");
+        }
+        fileSearcher.DisplayFolderStructure();
+        //fileSearcher.DeleteFilesInFolder(folderPath);
+
+
+
+    }
+}
